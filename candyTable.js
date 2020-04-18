19030c4ea94eb491bcd259c389b9c29e848de3fb
Bridgitte Ly <bridgittely@gmail.com>
2020-04-18 02:23:31 -0700

    Updated product pages

---
+++ candyTable.js
@@ -0,0 +1,41 @@
+// JS functions for onClick page linking
+
+function clickPB(){
+	location.href="peanut-butter-cups.html";
+};
+
+function clickCC(){
+	location.href="candy-canes.html";
+};
+
+function clickAJ(){
+	location.href="almond-joy.html";
+};
+
+function clickCO(){
+	location.href="candy-oreos.html";
+};
+
+function clickFR(){
+	location.href="fruit-rollups.html";
+};
+
+function clickNerds(){
+	location.href="nerds.html";
+};
+
+function clickSF(){
+	location.href="swedish-fish.html";
+};
+
+function clickRP(){
+	location.href="reeses-pieces.html";
+};
+
+function clickSkittles(){
+	location.href="skittles.html";
+};
+
+function clickRK(){
+	location.href="rice-krispies.html";
+};
