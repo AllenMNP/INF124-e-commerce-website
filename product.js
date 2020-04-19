82e1b2bda4641f79e5c2368b1ff4330e245f5877
Bridgitte Ly <bridgittely@gmail.com>
2020-04-19 01:14:51 -0700

    Can hover over product images to increase size

---
+++ product.js
@@ -0,0 +1,13 @@
+function bigImg(x){
+    x.style.height = "415px";
+    x.style.width = "415px";
+}
+
+function normalImg(x){
+    x.style.height = "300px";
+    x.style.width = "300px";
+}
+
+function orderProduct(){
+    location.href="order-page.html";
+}
