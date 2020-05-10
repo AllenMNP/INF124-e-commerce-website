9b6631ecc8570f6d1a5da536686719c84800b76a
Bridgitte Ly <bridgittely@gmail.com>
2020-05-10 02:50:17 -0700

    Tax, shipping, and total will update when changed

--- order-page.js
+++ order-page.js
@@ -40,7 +40,30 @@ function getPlace(zip){
     xhr.send(null);
   }
   
-  function run() {
-    document.getElementById("shipping").value == document.getElementById("shipment").value;
-    console.log(document.getElementById("shipping").innerHTML = document.getElementById("shipment").value)
+  function getPrice(product){
+    if (window.XMLHttpRequest){  
+       var xhr = new XMLHttpRequest();
+    }
+    else{  
+       var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhr.onreadystatechange = function(){ 
+      if (xhr.readyState == 4 && xhr.status == 200){ 
+        //var result = xhr.responseText;
+        //var place = result.split(', ');
+        if (document.getElementById("subtotal").value == "")
+          document.getElementById("subtotal").value = this.responseText;
+      }
+    }
+    xhr.open("GET", "getTax.php?zip=" + zip);
+    xhr.send(null);
   }
+  
+  function getTotal() {
+    var f1 = parseInt(document.getElementById('tax').value);
+    var f2 = parseInt(document.getElementById('shipping').value);
+    var res = f1 + f2;
+    document.getElementById('total').value = res;
+  }
+  
+  
